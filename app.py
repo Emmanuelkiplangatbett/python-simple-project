f9798c0fb30b9e2c7cfb98f39915dedd60d3ad37
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-08-25 17:51:11 +0300

    done with weight project

---
+++ app.py
@@ -0,0 +1,151 @@
+# name = input('what is your name? ')
+# favorite_color = input('what is your favorite_color? ')
+# # print( + name)
+# print(name +  ' likes ' + favorite_color)
+
+
+
+
+# birth_year = input('Birth year: ')
+# print (type(birth_year))
+# age = 2023 - int(birth_year)
+# print(type(age))
+# print(age)
+
+
+
+# weight_lbs = input('weight (lbs): ')
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
+# course = '''
+
+# Hi John,
+
+# Here is our first email to you.
+
+
+# Thank you,
+# The support team
+
+
+# '''
+
+# print(course)
+
+
+# course = 'python for Beginners'
+
+# print(course[1:])
+
+
+# name = 'Jennifer'
+
+# print(name[1:-2])
+
+
+
+
+      # */FORMATTED STRINGS/*
+# first = 'John'
+# last = 'Smith'
+
+#  message = first + ' [' + last + '] is a coder'
+# msg = f'{first} [{last}] is a coder'
+
+# print(msg)
+
+   #STRING METHODS
+# print(20 % 3)
+# course = 'Python for Beginners'
+# print(len(course)) len function is used t count no of characters in a string
+
+# print(course.upper())
+# print(course.lower())
+
+# print(course.find('P')) ..its find the index of character P
+# print(course.replace('Beginners', 'Absolute Beginners'))
+# IN operator
+
+# print('Python' in course) returns boolean value  "true"
+
+
+       #ARITHMETIC OPERATERS
+#  print(10 ** 3) =>1000
+# print(20 % 3) =>2
+
+# x = 10
+# x = x + 3 or
+# x += 3  =>13  agmented assignment operator
+# print(x)
+
+  # OPERATOR PROCEDENCE
+
+# x = 10 + 3 * 2
+# print(x) =>16
+
+
+
+    # MATH FUNCTIONS
+# x = 2.9
+# print(round(x))
+
+# print(abs(-2.9))
+
+# import math
+# print (math.floor(2.9))=>2
+# print (math.ceil(2.9))=>3
+
+        # IF STATEMENTS
+# is_hot = False
+# is_cold = False
+
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clothes")
+# else:
+#     print ("It's a lovely day")
+
+# print("Enjoy your day")
+
+
+
+
+# # Check buyer's credit
+# buyer_has_good_credit = True  # Change to False if needed
+
+# # Calculate price and print
+# price = 1000000
+# buyer_has_good_credit = True  # Change to False if needed
+# if buyer_has_good_credit:
+#     down_payment = 0.1 * price
+# else:
+#     down_payment = 0.2 * price
+# print(f"Down payment: ${down_payment}")
+
+
+    # LOGICAL OPERATORS
+    # logical{AND}
+# has_high_income = True
+# has_good_credit = True
+
+# if has_high_income and has_good_credit:
+#     print("Eligible for loan")
+
+    # with logical{and} operator both conditions should be true 
+    # with logical{or} operator atleast one condition should be true
+    # ....logical{not} operator  has two true values  hence  
+
+
+# has_good_credit = True
+# has_criminal_record = False
+
+# if has_good_credit and not has_criminal_record:
+#     print("Eligible for loan")
+
+
+
+         #  COPMARISON OPERATORS
